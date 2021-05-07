923f82ed8917a0abe83f4d1528b28979ee0e348a
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-07 17:12:49 -0300

    adiciona fonte tipografica roboto-condensed via google fonts

--- App.js
+++ App.js
@@ -4,16 +4,28 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
 import { useCachedResources } from 'react-native-rapi-ui';
+import {
+	useFonts,
+	RobotoCondensed_700Bold_Italic,
+	RobotoCondensed_400Regular,
+	RobotoCondensed_400Regular_Italic,
+	RobotoCondensed_700Bold
+} from '@expo-google-fonts/roboto-condensed';
 
-export default function App(props) {
+export default function App (props) {
 	const isLoadingComplete = useCachedResources();
-
-	if (!isLoadingComplete && !props.skipLoadingScreen) {
+	let [fontsLoaded] = useFonts({
+		RobotoCondensed_700Bold_Italic,
+		RobotoCondensed_400Regular,
+		RobotoCondensed_400Regular_Italic,
+		RobotoCondensed_700Bold
+	});
+	if (!isLoadingComplete &&  !props.skipLoadingScreen || !fontsLoaded) {
 		return <AppLoading />;
 	} else {
 		return (
 			<SafeAreaView style={{ flex: 1 }}>
-				<StatusBar style="auto" translucent />
+				<StatusBar style='auto' translucent />
 				<AppNavigator />
 			</SafeAreaView>
 		);
