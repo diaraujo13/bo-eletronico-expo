9abdfa186e8c9855ba5d5612460a3e84323997df
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-13 21:59:12 -0300

    updates

---
+++ src/components/FormWrapper.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	TouchableWithoutFeedback,
+} from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+
+const FormWrapper = (props) => {
+    return <>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView>
+            <KeyboardAvoidingView
+                behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+            >
+                {props.children}
+                <KeyboardSpacer />
+            </KeyboardAvoidingView>
+        </ScrollView>
+    </TouchableWithoutFeedback>
+    </>
+};
+
+
+export default FormWrapper;
--- src/screens/NewBO.js
+++ src/screens/NewBO.js
@@ -1,54 +1,24 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-rapi-ui';
+import { Formik } from 'formik'
+import React, { Component, useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { TextInput } from 'react-native-rapi-ui'
+import FormWrapper from '../components/FormWrapper'
 
-// create a component
 const NewBO = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 27, fontFamily: "RobotoCondensed_700Bold" , marginBottom: 10 }}>BOLETIM DE OCORRÊNCIA - POLÍCIA MILITAR</Text>
+  const [loading, setLoading] = useState(false)
 
-            
+  return (
+    <>
+      <FormWrapper>
+        <Formik
+          initialValues={{}}
+          onSubmit={values => {
+            console.warn(values)
+          }}
+        ></Formik>
+      </FormWrapper>
+    </>
+  )
+}
 
-         
-
-            <Text style={{ fontSize: 20, fontFamily: "RobotoCondensed_400Regular" , marginBottom: 10 }}>UNIDADE RESPONSÁVEL</Text>
-            <TextInput
-                placeholder="Enter your text"
-            />
-
-            <View style={{backgroundColor: 'gray', padding: 5, marginVertical: 5, color: 'white'}}>
-                <Text style={{ fontSize: 20, color: 'white', textAlign:'center', fontFamily: "RobotoCondensed_400Regular" , marginBottom: 10 }}>DADOS DA OCORRÊNCIA</Text>
-            </View>
-
-            <Text style={{ fontSize: 20, fontFamily: "RobotoCondensed_400Regular" , marginBottom: 10 }}>UNIDADE RESPONSÁVEL</Text>
-            <TextInput
-                placeholder="Enter your text"
-            />
-
-<Text style={{ fontSize: 20, fontFamily: "RobotoCondensed_400Regular" , marginBottom: 10 }}>UNIDADE RESPONSÁVEL</Text>
-            <TextInput
-                placeholder="Enter your text"
-            />
-
-<Text style={{ fontSize: 20, fontFamily: "RobotoCondensed_400Regular" , marginBottom: 10 }}>UNIDADE RESPONSÁVEL</Text>
-            <TextInput
-                placeholder="Enter your text"
-            />
-
-            
-        </View>
-    );
-};
-
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 8
-    },
-});
-
-//make this component available to the app
-export default NewBO;
+export default NewBO
