11de51ac3b0a8753224e0c849709637998696493
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-14 17:58:36 -0300

    headers

--- src/screens/NewBO.js
+++ src/screens/NewBO.js
@@ -5,44 +5,61 @@ import { TextInput, Button } from 'react-native-rapi-ui'
 import FormField from '../components/form/FormField'
 import FormWrapper from '../components/form/FormWrapper'
 import { formSchema } from '../helpers/formSchema'
+import { Layout, TopNav } from 'react-native-rapi-ui'
+import { Section, SectionContent } from 'react-native-rapi-ui'
+import { Ionicons } from '@expo/vector-icons'
 
-const NewBO = () => {
+const NewBO = ({ navigation }) => {
   const [form, setForm] = useState(formSchema)
   const [loading, setLoading] = useState(false)
 
   return (
-    <View style={{ padding: 10 }}>
-      <Text
-        style={{
-          fontSize: 27,
-          fontFamily: 'RobotoCondensed_700Bold',
-          marginBottom: 10
-        }}
-      >
-        BOLETIM DE OCORRÊNCIA - POLÍCIA MILITAR
-      </Text>
-      <FormWrapper>
-        <Formik
-          initialValues={{}}
-          onSubmit={values => {
-            console.log(JSON.stringify(values))
-          }}
-        >
-          {formikObj => (
-            <>
-              {formSchema.map((formField, i) => (
-                <FormField attrs={formField} key={i} formikObj={formikObj} />
-              ))}
-              <Button
-                loading={loading}
-                onPress={formikObj.handleSubmit}
-                text='Salvar'
-              />
-            </>
-          )}
-        </Formik>
-      </FormWrapper>
-    </View>
+    <Layout>
+      <TopNav
+        leftContent={<Ionicons name='chevron-back' size={20} color={'#000'} />}
+        leftAction={() => navigation.goBack()}
+        middleContent='Formulário'
+      />
+
+      <Section>
+        <SectionContent>
+          <Text
+            style={{
+              fontSize: 27,
+              fontFamily: 'RobotoCondensed_700Bold',
+              marginBottom: 10
+            }}
+          >
+            BOLETIM DE OCORRÊNCIA - POLÍCIA MILITAR
+          </Text>
+          <FormWrapper>
+            <Formik
+              initialValues={{}}
+              onSubmit={values => {
+                console.log(JSON.stringify(values))
+              }}
+            >
+              {formikObj => (
+                <>
+                  {formSchema.map((formField, i) => (
+                    <FormField
+                      attrs={formField}
+                      key={i}
+                      formikObj={formikObj}
+                    />
+                  ))}
+                  <Button
+                    loading={loading}
+                    onPress={formikObj.handleSubmit}
+                    text='Salvar'
+                  />
+                </>
+              )}
+            </Formik>
+          </FormWrapper>
+        </SectionContent>
+      </Section>
+    </Layout>
   )
 }
 
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Layout, Text } from 'react-native-rapi-ui';
+import React from 'react'
+import { View } from 'react-native'
+import { Layout, Text } from 'react-native-rapi-ui'
+import { TopNav } from 'react-native-rapi-ui'
 
 export default function ({ navigation }) {
-	return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Text>This is the Profile tab</Text>
-			</View>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <TopNav middleContent='Procurar' />
+      <View
+        style={{
+          flex: 1,
+          padding: 20
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed_400Regular',
+            marginBottom: 10,
+            textTransform: 'uppercase'
+          }}
+        >
+          PROCURAR
+        </Text>
+      </View>
+    </Layout>
+  )
 }
