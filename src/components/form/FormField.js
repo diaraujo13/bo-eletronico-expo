de09339c7d06e059d030dd354518093be7be2260
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-21 17:17:39 -0300

    new form input for time selection

--- src/components/form/FormField.js
+++ src/components/form/FormField.js
@@ -1,85 +1,88 @@
 //FormikTextInputt liraries
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { TextInput } from 'react-native-rapi-ui'
-import FormDatePicker from './inputs/FormDatePicker'
-import FormTimePicker from './inputs/FormTimePicker'
-import FormSelect from './inputs/FormSelect'
-import FormDuoSelect from './inputs/FormDuoSelect'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-rapi-ui';
+import FormDatePicker from './inputs/FormDatePicker';
+import FormTimePicker from './inputs/FormTimePicker';
+import FormSelect from './inputs/FormSelect';
+import FormDuoSelect from './inputs/FormDuoSelect';
 
 // create a component
 const FormField = ({ attrs, formikObj }) => {
-  const { title, subtitle, placeholder, name, ...otherProps } = attrs
-  const { handleChange, values } = formikObj
+	const { title, subtitle, placeholder, name, ...otherProps } = attrs;
+	const { handleChange, values } = formikObj;
 
-  const renderInputComponent = () => {
-    switch (attrs.inputType) {
-      case 'text':
-        return (
-          <TextInput
-            placeholder={placeholder}
-            underlineColorAndroid='rgba(0,0,0,0)'
-            onChangeText={handleChange(name)}
-            value={values[name]}
-            {...formikObj}
-          />
-        )
-        break
-      case 'date':
-        return <FormDatePicker attrs={attrs} value={values[name]} {...formikObj} />
-        break
-      case 'select':
-        return (
-          <FormSelect
-            attrs={attrs}
-            {...formikObj}
-            {...otherProps}
-          />
-        )
-        break
-      case 'duo_select':
-        return (
-          <FormDuoSelect
-            attrs={attrs}
-            value={values[name]}
-            {...formikObj}
-            {...otherProps}
-          />
-        )
-        break
-      default:
-        break
-    }
-  }
+	const renderInputComponent = () => {
+		switch (attrs.inputType) {
+		case 'text':
+			return (
+				<TextInput
+					placeholder={placeholder}
+					underlineColorAndroid='rgba(0,0,0,0)'
+					onChangeText={handleChange(name)}
+					value={values[name]}
+					{...formikObj}
+				/>
+			);
+			break;
+		case 'date':
+			return <FormDatePicker attrs={attrs} value={values[name]} {...formikObj} />;
+			break;
+		case 'time':
+			return <FormTimePicker attrs={attrs} {...formikObj} />;
+			break;
+		case 'select':
+			return (
+				<FormSelect
+					attrs={attrs}
+					{...formikObj}
+					{...otherProps}
+				/>
+			);
+			break;
+		case 'duo_select':
+			return (
+				<FormDuoSelect
+					attrs={attrs}
+					value={values[name]}
+					{...formikObj}
+					{...otherProps}
+				/>
+			);
+			break;
+		default:
+			break;
+		}
+	};
 
-  return (
-    <View>
-      {title && <Text style={styles.form_field_label}>{title}</Text>}
-      {subtitle && <Text style={styles.form_field_subtitle}>{subtitle}</Text>}
-      <View style={styles.px10}>{renderInputComponent()}</View>
-    </View>
-  )
-}
+	return (
+		<View>
+			{title && <Text style={styles.form_field_label}>{title}</Text>}
+			{subtitle && <Text style={styles.form_field_subtitle}>{subtitle}</Text>}
+			<View style={styles.px10}>{renderInputComponent()}</View>
+		</View>
+	);
+};
 
 // define your styles
 const styles = StyleSheet.create({
-  px10: { paddingVertical: 10 },
-  form_field_label: {
-    fontSize: 13,
-    fontFamily: 'RobotoCondensed_400Regular',
-    marginBottom: 10,
-    textTransform: 'uppercase'
-  },
-  form_field_subtitle: {
-    fontSize: 11,
-    color: 'gray',
-    fontFamily: 'RobotoCondensed_400Regular',
-    marginBottom: 10
-  },
-  container: {
-    flex: 1
-  }
-})
+	px10: { paddingVertical: 10 },
+	form_field_label: {
+		fontSize: 13,
+		fontFamily: 'RobotoCondensed_400Regular',
+		marginBottom: 10,
+		textTransform: 'uppercase'
+	},
+	form_field_subtitle: {
+		fontSize: 11,
+		color: 'gray',
+		fontFamily: 'RobotoCondensed_400Regular',
+		marginBottom: 10
+	},
+	container: {
+		flex: 1
+	}
+});
 
 //make this component available to the app
-export default FormField
+export default FormField;
--- src/components/form/inputs/FormTimePicker.js
+++ src/components/form/inputs/FormTimePicker.js
@@ -1,25 +1,34 @@
 //import liraries
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text';
 
 // create a component
-const FormTimePicker = () => {
-  return (
-    <View style={styles.container}>
-      <Text>FormTimePicker</Text>
-    </View>
-  )
-}
+const FormTimePicker = props => {
+	const { 
+		attrs: { name},   
+		formikObj: {
+			handleChange,  values
+		} } = props;
+
+	return (
+		<View style={styles.container}>
+			<TextInputMask
+				type={'HH:mm'}
+				onChangeText={handleChange(name)}
+				value={values[name]}
+			/>
+		</View>
+	);
+};
 
 // define your styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50'
-  }
-})
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+	}
+});
 
 //make this component available to the app
-export default FormTimePicker
+export default FormTimePicker;
