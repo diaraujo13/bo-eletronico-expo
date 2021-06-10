5e3d5599967688c2b74fef5857b8d7a25bc52df9
Izaías di Araújo <izaiasousa@gmail.com>
2021-06-10 11:45:27 -0300

    atualizaçãõ de mensagem da terceira tela

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Layout, Text } from 'react-native-rapi-ui';
+import React from 'react'
+import { View } from 'react-native'
+import { Layout, Text } from 'react-native-rapi-ui'
 
 export default function ({ navigation }) {
-	return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Text>This is the About tab</Text>
-			</View>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text>Em breve mais informações</Text>
+      </View>
+    </Layout>
+  )
 }
