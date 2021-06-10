dac81ae2a38a61c8e9a220c7836e457f42158bea
Izaías di Araújo <izaiasousa@gmail.com>
2021-06-10 11:35:33 -0300

    lista de formularios

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native'
 import { Layout, Text } from 'react-native-rapi-ui'
 import { TopNav } from 'react-native-rapi-ui'
+import { API_URL } from '../helpers/constants'
+import { Ionicons } from '@expo/vector-icons'
+import { Section, SectionContent, SectionImage } from 'react-native-rapi-ui'
 
 export default function ({ navigation }) {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    fetch(API_URL + '/boletim')
+      .then(res => res.json())
+      .then(res => {
+        setItems(res.result)
+        console.log(res.result)
+      })
+  }, [])
   return (
     <Layout>
       <TopNav middleContent='Procurar' />
@@ -23,7 +35,74 @@ export default function ({ navigation }) {
         >
           PROCURAR
         </Text>
+
+        <FlatList
+          data={items}
+          style={{ flex: 1 }}
+          keyExtractor={el => el._id}
+          renderItem={({ item }) => (
+            <>
+              <Section style={styles.container}>
+                <SectionContent
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <View>
+                    <Text size='h3'> {item.matricula}</Text>
+                    <Text size='h5'> {item.responsavel}</Text>
+                    <Text size='h6'> {item.createdAt}</Text>
+                    <Text size='h6'>COD: {item._id}</Text>
+                  </View>
+
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '',
+                      justifyContent: 'center',
+                      flexDirection: 'column',
+                      alignSelf: 'flex-end',
+                      color: '#fff',
+                      top: '0%'
+                    }}
+                    onPress={() => {
+                      console.log('Edit ex')
+                    }}
+                  >
+                    <Ionicons
+                      size={25}
+                      name='information-circle'
+                      color='#535353'
+                    />
+                  </TouchableOpacity>
+                </SectionContent>
+              </Section>
+            </>
+          )}
+        />
       </View>
     </Layout>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+    marginTop: 10
+  },
+  flatListContainer: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    backgroundColor: '#ffffff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5
+  }
+})
