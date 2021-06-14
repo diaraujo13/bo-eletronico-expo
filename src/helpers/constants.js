ead151ced5e218ba72e70c0b1d258efc7b930a00
Izaías di Araújo <izaiasousa@gmail.com>
2021-06-14 19:44:05 -0300

    atualizacao da url para api

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,9 @@
+import  Env from 'react-native-config';
+
 export const API_URL =
-  process.env.API_URL || 'https://bo-eletronico-app.herokuapp.com/api/v1'
+    process.env["NODE_ENV"] == 'development' ?
+        "http://localhost:3000/api/v1" :
+                'https://bo-eletronico-app.herokuapp.com/api/v1'
+
 
 export const MAPS_API_KEY = process.env.MAPS_API_KEY || ''
